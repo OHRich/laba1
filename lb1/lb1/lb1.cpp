0d5315ccbf7d650c0dbe2e8cdc472f527a2a35d4
OHRich <sushevpro75@gmail.com>
2022-09-12 21:20:56 +0300

    Initial commit

---
+++ lb1/lb1/lb1.cpp
@@ -0,0 +1,74 @@
+﻿// lb1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int** a, temp, min, max, res, n, m, sum;
+    srand(time(NULL));
+    cout << "Введите количество строк: ";
+    cin >> n;
+    cout << "Введите количество столбцов: ";
+    cin >> m;
+    a = (int**)malloc(n * sizeof(int*)); //выделяем память под строку указателей
+    for (int i = 0; i < n; i++) {
+        *(a + i) = (int*)malloc(m * sizeof(int)); //выделяем память под значения столбцов
+    }
+    for (int i = 0; i < n; i++)  // цикл по столбцам
+    {
+        for (int j = 0; j < m; j++)  // цикл по строкам
+        {
+            *(*(a + i) + j) = rand() % 100;
+            cout.width(3);
+            cout << *(*(a + i) + j);
+        }
+        cout << endl;
+    }
+    min = **a;
+    max = **a;
+    cout << "\nСуммы строк: " << endl;
+    for (int i = 0; i < n; i++)  // цикл по столбцам
+    {
+        sum = 0;
+        for (int j = 0; j < m; j++)  // цикл по строкам
+        {
+            temp = *(*(a + i) + j);
+            sum = sum + temp;
+            if (temp < min) {
+                min = temp;
+            }
+            if (temp > max) {
+                max = temp;
+            }
+        }
+        cout << sum << endl;
+    }
+    cout << "\nСумма стобцов: " << endl;
+    for (int j = 0; j < m; j++)  // цикл по строкам
+    {
+        sum = 0;
+        for (int i = 0; i < n; i++)  // цикл по столбцам
+        {
+            temp = *(*(a + i) + j);
+            sum = sum + temp;
+            if (temp < min) {
+                min = temp;
+            }
+            if (temp > max) {
+                max = temp;
+            }
+        }
+        cout << sum << endl;
+    }
+    res = max - min;
+    cout << "\nРазница между максимальным и минимальным элементом массива: " << max << "-" << min << "=" << res << endl;
+    for (int i = 0; i < n; i++) {
+        free(*(a + i)); //освобождаем память
+    }
+    free(a); //освобождаем память
+}
---
+++ lb1pt2/lb1pt2/lb1pt2.cpp
@@ -0,0 +1,51 @@
+﻿// lb1pt2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int sravnstr(char str1[20], char str2[20]) {
+    for (int i = 0; i < 20; i++) {
+        if (str1[i] != str2[i]) {
+            return 0;
+        }
+    }
+    return 1;
+}
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    system("chcp 1251");
+    system("cls");
+    struct student
+    {
+        char name[20], surname[20], facult[20];
+        int nomzach;
+    } students[5];
+    char fam[20], nam[20], flag = 0;
+    for (int i = 0; i < 5; i++) {
+        cout << "Введите фамилию студента: ";
+        cin >> students[i].surname;
+        cout << "Введите имя студента: ";
+        cin >> students[i].name;
+        cout << "Введите факультет: ";
+        cin >> students[i].facult;
+        cout << "Введите номер зачётки: ";
+        cin >> students[i].nomzach;
+        cout << endl;
+    }
+    cout << endl << "Введите фамилию студента для поиска: ";
+    cin >> fam;
+    cout << endl << "Введите имя студента для поиска: ";
+    cin >> nam;
+    for (int i = 0; i < 5; i++) {
+        if (sravnstr(students[i].name, nam) && sravnstr(students[i].surname, fam)) {
+            cout << endl << students[i].surname << " " << students[i].name << " " << students[i].facult << " " << students[i].nomzach;
+            flag = 1;
+        }
+    }
+    if (flag == 0) {
+        cout << "Ничего не найдено";
+    }
+}
